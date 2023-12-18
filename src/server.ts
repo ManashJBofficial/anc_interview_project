a9877a9cb02e82b28d6fda087a774ce3e47799e0
Manash Jy Baruah <mjbaruah10@gmail.com>
2023-12-18 21:35:47 +0530

    server setup initial commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = Number(process.env.PORT) || 5000;
+
+app.use(express.json());
+
+app.get("/api", (_req: express.Request, res: express.Response) => {
+  res.status(200).json({
+    films: "https://swapi.dev/api/films/",
+    people: "https://swapi.dev/api/people/",
+    planets: "https://swapi.dev/api/planets/",
+    species: "https://swapi.dev/api/species/",
+    starships: "https://swapi.dev/api/starships/",
+    vehicles: "https://swapi.dev/api/vehicles/",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
