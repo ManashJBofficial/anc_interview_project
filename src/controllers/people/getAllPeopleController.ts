93db6ef3f30f78539da5fa1f7fdbc8f89d240993
Manash Jy Baruah <mjbaruah10@gmail.com>
2023-12-19 11:19:58 +0530

    added ttl for redis cache

--- src/controllers/people/getAllPeopleController.ts
+++ src/controllers/people/getAllPeopleController.ts
@@ -14,10 +14,17 @@ const getAllPeople = asyncHandler(async (req: Request, res: Response) => {
   const response = await axios.get(`${SWAPI_BASE_URL}/people`, {
     params: { page },
   });
+
   //store response.data to redis cache
+  const TTL_SECONDS = 30 * 60;
+
   const redisClient = await connect();
+
   redisClient.set(key, JSON.stringify(response.data));
+  redisClient.expire(key, TTL_SECONDS);
+
   console.log("Data retrieved from API");
+
   res.json(response.data);
 });
 
